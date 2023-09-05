34170de0d84b2d8d45915fbcb5a969f02a63548f
deborapatrao <deborapcorreia@gmail.com>
2023-09-05 09:48:59 -0700

    Client: add delete client

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "./assets/logo.png";
 export default function Header() {
   return (
     <nav className="navbar bg-light mb-4 p-0">
-      <div className="conatiner">
+      <div className="container">
         <a className="navbar-brand" href="#">
           <div className="d-flex">
             <img src={logo} alt="logo" className="mr-2" />
